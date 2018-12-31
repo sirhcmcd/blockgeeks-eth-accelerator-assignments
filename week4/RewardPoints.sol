a669205055ef432b21242b3bb40a4fa9e7e1420f
sirhcmcd <chris@mcdaniel.com>
2018-12-31 03:25:09 -0800

    week 4 updates

--- week4/RewardPoints.sol
+++ week4/RewardPoints.sol
@@ -1,6 +1,11 @@
 pragma solidity ^0.4.24;
 
+import "./SafeMath.sol";
+
 contract RewardPoints {
+    
+    using SafeMath for uint256;
+
     address private owner;
     mapping(address => bool) private isAdmin; // Quick way to check if an addr is an Admin
 
@@ -113,10 +118,18 @@ contract RewardPoints {
     // =================================
     function addAdmin(address _admin) external onlyOwner {
         // TODO: your code here
+        
+        isAdmin[_admin] = true; 
+        emit AddedAdmin(_admin);
+
     }
 
     function removeAdmin(address _admin) external onlyOwner {
         // TODO: your code here
+        
+        isAdmin[_admin] = false;
+        emit RemovedAdmin(_admin);
+
     }
 
     // =================================
@@ -125,37 +138,96 @@ contract RewardPoints {
     function addMerchant(address _merchant) external onlyAdmin {
         // TODO: your code here
         // Hints: Remember the index into the array is the ID
+        
         // 1. Create a new merchant and assign various fields
+        Merchant memory newMerchant;
+        uint256 newId = merchants.length; 
+        newMerchant.id = newId;
+        newMerchant.addr = _merchant;
+        newMerchant.isApproved = true;
+        
         // 2. Push new merchant into array
+        merchants.push(newMerchant);
+
         // 3. Update addrToMerchantId mapping
+        addrToMerchantId[_merchant] = newId;
+
         // 4. Emit event
+        emit AddedMerchant(_merchant,newId);
+
+        
     }
 
     function banMerchant(uint _id) external onlyAdmin {
         // TODO: your code here
         // Hints: Only ban merchants that are valid and
         // remember we're not removing a merchant.
+        
+        require(merchants[_id].isApproved);   // only approved merchants 
+
+        merchants[_id].isApproved = false;
+        emit BannedMerchant(_id);
+        
     }
 
     function approveMerchant(uint _id) external onlyAdmin {
         // TODO: your code here
         // Hints: Do the reverse of banMerchant
+        
+        require(!merchants[_id].isApproved);  // only not approved merchants
+        
+        merchants[_id].isApproved = true;
+        emit ApprovedMerchant(_id);
+        
     }
 
     function addUser(address _user) external onlyAdmin {
         // TODO: your code here
         // Hints: Similar steps to addMerchant
+        
+        // 1. Create a new user and assign various fields
+        User memory newUser;
+        uint256 newId = users.length; 
+        newUser.id = newId;
+        newUser.addr = _user;
+        newUser.isApproved = true;
+        newUser.totalEarnedPoints = 0;
+        newUser.totalReedemedPoints = 0;
+        
+        // 2. Push new user into array
+        users.push(newUser);
+        
+        // 3. Update addrToUserId mapping
+        addrToUserId[_user] = newId;
+        
+        // 4. Emit event
+        emit AddedUser(_user,newId);
+        
     }
 
     function banUser(address _user) external onlyAdmin {
         // TODO: your code here
         // Hints: Similar to banMerchant but the input
         // parameter is user address instead of ID.
+        
+        uint userId = addrToUserId[_user];  // get userId 
+        require(users[userId].isApproved);  // only approved users 
+        
+        users[userId].isApproved = false;
+        emit BannedUser(_user, userId);
+        
     }
 
     function approveUser(address _user) external onlyAdmin {
         // TODO: your code here
         // Hints: Do the reverse of banUser
+        
+        uint userId = addrToUserId[_user];
+        require(!users[userId].isApproved);
+        
+        users[userId].isApproved = true;
+        emit ApprovedUser(_user, userId);
+        
     }
 
     // =================================
@@ -164,15 +236,37 @@ contract RewardPoints {
     function addOperator(address _operator) external onlyMerchantOwner {
         // TODO: your code here
         // Hints:
+        
         // 1. Get the merchant ID from msg.sender
+        uint merchantId = addrToMerchantId[msg.sender];
+
         // 2. Set the correct field within the Merchant Struct
+        merchants[merchantId].isOperator[_operator] = true;
+
         // 3. Update addrToMerchantId mapping
+        addrToMerchantId[_operator] = merchantId;
+        
         // 4. Emit event
+        emit AddedOperator(merchantId, _operator);
+        
     }
 
     function removeOperator(address _operator) external onlyMerchantOwner {
         // TODO: your code here
         // Hints: Do the reverse of addOperator
+        
+        // 1. Get the merchant ID from msg.sender
+        uint merchantId = addrToMerchantId[msg.sender];
+        
+        // 2. Set the correct field within the Merchant Struct
+        merchants[merchantId].isOperator[_operator] = false;
+        
+        // 3. Update addrToMerchantId mapping
+        addrToMerchantId[_operator] = 0;
+        
+        // 4. Emit event
+        emit RemovedOperator(merchantId, _operator);
+        
     }
 
     function transferMerchantOwnership(address _newAddr) external onlyMerchantOwner {
@@ -180,6 +274,21 @@ contract RewardPoints {
         // Hints: Similar to addOperator but update different fields
         // but remember to update the addrToMerchantId twice. Once to
         // remove the old owner and once for the new owner.
+        
+        // 1. Get the merchant ID from msg.sender
+        uint merchantId = addrToMerchantId[msg.sender];
+        
+        // 2. Set the correct field within the Merchant Struct
+        merchants[merchantId].isOperator[msg.sender] = false;
+        merchants[merchantId].isOperator[_newAddr] = true;
+        
+        // 3. Update addrToMerchantId mapping
+        addrToMerchantId[msg.sender] = 0;
+        addrToMerchantId[_newAddr] = merchantId;
+        
+        // 4. Emit event
+        emit TransferredMerchantOwnership(merchantId, msg.sender, _newAddr);
+        
     }
 
     // =================================
@@ -189,6 +298,15 @@ contract RewardPoints {
         // TODO: your code here
         // Hints: update the total and per merchant points
         // for the user in the User struct.
+        
+        uint merchantId = addrToMerchantId[msg.sender];
+        uint userId = addrToUserId[_user];
+        
+        users[userId].totalEarnedPoints.add(_points);
+        users[userId].merchantToEarnedPts[merchantId].add(_points);
+        
+        emit RewardedUser(_user, merchantId, _points);
+        
     }
 
     // =================================
@@ -197,10 +315,20 @@ contract RewardPoints {
     function redeemPoints(uint _mId, uint _points) external onlyUser {
         // TODO: your code here
         // Hints:
+        
         // 1. Get the user ID from caller
+        uint256 userId = addrToUserId[msg.sender];
+
         // 2. Ensure user has at least _points at merchant with id _mID
+        require(getUserEarnedPointsAtMerchant(msg.sender, _mId).sub(getUserRedeemedPointsAtMerchant(msg.sender, _mId)) >= _points);
+
         // 3. Update the appropriate fields in User structs
+        users[userId].totalReedemedPoints.add(_points);
+        users[userId].merchantToRedeemedPts[_mId].add(_points);
+
         // 4. Emit event
+        emit RedeemedPoints(msg.sender, _mId, _points);
+
     }
 
     // =================================
